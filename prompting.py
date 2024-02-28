c68c760f394865aad0a30fe42a8ef84a6cc73e53
Rishabh-14 <77384078+Rishabh-14@users.noreply.github.com>
2024-02-28 14:50:10 +0530

    Update prompting.py

--- prompting.py
+++ prompting.py
@@ -1,8 +1,9 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
+# Prompt template and LLM
 prompt = ChatPromptTemplate.from_template("tell me a joke about {foo}")
-model = ChatOpenAI(openai_api_key="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")
+model = ChatOpenAI()
 chain = prompt | model
 
 response = chain.invoke({"foo": "bears"})
@@ -10,6 +11,81 @@ print(response)
 
 chain = prompt | model.bind(stop=["\n"])
 
-response = chain.invoke({"foo": "bears"})
+#response = chain.invoke({"foo": "bears"})
 
+#print(response)
+
+# Function calling
+
+functions = [
+    {
+        "name": "joke",
+        "description": "A joke",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "setup": {"type": "string", "description": "The setup for the joke"},
+                "punchline": {
+                    "type": "string",
+                    "description": "The punchline for the joke",
+                },
+            },
+            "required": ["setup", "punchline"],
+        },
+    }
+]
+chain = prompt | model.bind(function_call={"name": "joke"}, functions=functions)
+
+#response = chain.invoke({"foo": "bears"}, config={})
+
+#print(response)
+
+# PromptTemplate + LLM + OutputParser
+
+
+from langchain_core.output_parsers import StrOutputParser
+
+chain = prompt | model | StrOutputParser()
+
+#response = chain.invoke({"foo": "bears"})
+
+#print(response)
+
+# Function output parser
+
+from langchain.output_parsers.openai_functions import JsonKeyOutputFunctionsParser
+from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser
+
+chain = (
+    prompt
+    | model.bind(function_call={"name": "joke"}, functions=functions)
+    | JsonOutputFunctionsParser()
+)
+
+#response = chain.invoke({"foo": "bears"})
+#print(response)
+
+#Simplifying input
+
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+
+
+map_ = RunnableParallel(foo=RunnablePassthrough())
+chain = (
+    prompt
+    | model.bind(function_call={"name": "joke"}, functions=functions)
+    | JsonKeyOutputFunctionsParser(key_name="setup")
+)
+
+#response = chain.invoke({"foo": "bears"})
+#print(response)
+
+chain = (
+    {"foo": RunnablePassthrough()}
+    | prompt
+    | model.bind(function_call={"name": "joke"}, functions=functions)
+    | JsonKeyOutputFunctionsParser(key_name="setup")
+)
+
+response = chain.invoke("bears")
 print(response)
